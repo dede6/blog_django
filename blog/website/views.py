6c360a0c0ffa87d3118dfb703986591faeee27d5
Filipe <filipedeusdev@gmail.com>
2020-03-27 11:21:46 -0300

    Comandos do sistema de templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello_blog(request):
+	lista = [
+	'Django', 'Python', 'Git', 'HTML',
+	 'DataBase', 'Nginx', 'Uwsgi', 'Systemctl'
+	 ]
+	data = {'name' : 'Curso de Django 3', 'lista_tech' : lista}
+	return render(request, 'index.html', data)
